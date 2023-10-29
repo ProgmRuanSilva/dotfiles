6db83a17ba4ba23f0939d241d88f206869f093f8
= <=>
2023-10-29 17:46:16 -0300

    👷 Adding command to backup all files

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,8 +1,9 @@
-cp -r ~/.config/bspwm/themes/own/* ~/dotfiles/
-cp -r ~/
-cp -r ~/
-cp -r ~/
-cp -r ~/
-cp -r ~/
-cp -r ~/
-cp -r ~/
+# Configuration for backup update
+cp -r ~/.config/bspwm/themes/own/* ~/dotfiles/bspwm/own/ ;
+cp -r ~/.config/bspwm/sxhkdrc ~/dotfiles/bspwm/ ;
+cp -r ~/.config/starship/starship.toml ~/dotfiles/zsh/starship/ ;
+cp -r ~/.config/nvim/lua/custom/* ~/dotfiles/nvim/nvchad/custom/ ;
+# cp -r ~/
+# cp -r ~/
+# cp -r ~/
+# cp -r ~/
