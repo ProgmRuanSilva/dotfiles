d1c61b19808e1fcef0256d10fcff0d9d04aadcab
Ruan Silva <progmruansilva@gmail.com>
2024-12-02 10:26:35 -0300

    ✨ (package.sh): New packages

--- bin/packages.sh
+++ bin/packages.sh
@@ -1,3 +1,3 @@
-yay -S sqlite libxml2 zlib gdbm nss openssl readline libffi xz yaml-cpp unzip make gcc wget curl fzf openssl libyaml ripgrep zsh imagemagick ffmpeg mupdf mupdf-tools poppler libxml2 git cmake mesa docker docker-compose docker-machine docker-buildx unixodbc sxhkd  fzf gyp bat github-cli microsoft-edge-beta neovim fd
+yay -S sqlite libxml2 zlib gdbm nss openssl readline libffi xz yaml-cpp unzip make gcc wget curl fzf openssl libyaml ripgrep zsh imagemagick ffmpeg mupdf mupdf-tools poppler libxml2 git cmake mesa docker docker-compose docker-machine docker-buildx unixodbc sxhkd  fzf gyp bat github-cli microsoft-edge-beta neovim fd alacritty-smooth-cursor-git
 
 mkdir ~/projects
