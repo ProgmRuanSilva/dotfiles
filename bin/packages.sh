e9ac57b571d487fa9ad7a5e966f10f7980ac2e7d
Ruan SIlva <progmruansilva@gmail.com>
2026-05-04 16:10:48 -0300

    ✨ (feat): Tahoe icons default

--- bin/packages.sh
+++
@@ -1,1 +0,0 @@
-yay -S yaml-cpp unzip make gcc wget curl fzf libyaml ripgrep zsh git cmake mesa docker docker-compose docker-machine docker-buildx unixodbc sxhkd gyp bat fakeroot pkgconf neovim fd btop caraspace-bin
--- bin/update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-update () {
-  yay -Syu
-}
-
-update
